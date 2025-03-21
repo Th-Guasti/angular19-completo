e56c562f99d4c66b2822184a7e13e19466c7ba00
Th-Guasti <thaslesouza@gmail.com>
2025-03-21 13:10:29 -0300

    trabalhando com classe de modelo e arrays

--- projetos/conceitos-iniciais/src/app/lista-compras/lista-compras.component.ts
+++ projetos/conceitos-iniciais/src/app/lista-compras/lista-compras.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -9,9 +10,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class ListaComprasComponent {
   item: string = '';
+  lista: ItemLista[] = [];
 
   addItem() {
-    console.log("Item recebido: ", this.item);
+    let itemLista = new ItemLista();
+
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista);
   }
 }
