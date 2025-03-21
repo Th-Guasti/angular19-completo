04bafb5129bdcbfa21d796b702361be438845a00
Th-Guasti <thaslesouza@gmail.com>
2025-03-21 11:44:23 -0300

    adicionando ação no componente

--- projetos/conceitos-iniciais/src/app/calculadora/calculadora.component.ts
+++ projetos/conceitos-iniciais/src/app/calculadora/calculadora.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
-export class CalculadoraComponent {
 
+export class CalculadoraComponent {
+  numero1: number = 0; //ou numero1 ?: number
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    console.log("chamando o método para calcular o resultado!");
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
